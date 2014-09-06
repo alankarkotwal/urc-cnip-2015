d6a173a10031c31154c6791666b7055f5c2e53bb
Alankar Kotwal <alankarkotwal13@gmail.com>
2014-09-06 18:33:30 +0530

    removed build and devel

--- catkin_ws/build/rozebo_demo/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/singularity/Documents/MSI/urc-cnip-2015/catkin_ws/src/rozebo_demo
-# Build directory: /home/singularity/Documents/MSI/urc-cnip-2015/catkin_ws/build/rozebo_demo
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
