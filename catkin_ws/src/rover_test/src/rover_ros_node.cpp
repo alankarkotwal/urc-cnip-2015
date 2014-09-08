37eca1886896a7be69e716ad719b80c534b38e75
Alankar Kotwal <alankarkotwal13@gmail.com>
2014-09-08 15:14:39 +0530

    Added rover model test

---
+++ catkin_ws/src/rover_test/src/rover_ros_node.cpp
@@ -0,0 +1,53 @@
+#include <ros/ros.h>
+#include <gazebo_msgs/ModelStates.h>
+#include <stdlib.h>
+#define INDEX 0
+
+float t_vel_2 = 4;
+
+ros::Publisher publisher;
+rover_test::RoverCommands tx_msg;
+
+void RoverStateCallback(const gazebo_msgs::ModelStates::ConstPtr& rx_msg) {
+	if (rx_msg->name[INDEX] != "rover") { // TODO: Search for rover in general
+		ROS_ERROR("Rover not found at specified index in '/gazebo/model_states'");
+		exit (EXIT_FAILURE);
+	}
+	ROS_INFO("Position    ( %f, %f, %f )", rx_msg->pose[INDEX].position.x, rx_msg->pose[INDEX].position.y, rx_msg->pose[INDEX].position.z);
+	ROS_INFO("Orientation ( %f, %f, %f, %f )", rx_msg->pose[INDEX].orientation.x, rx_msg->pose[INDEX].orientation.y, rx_msg->pose[INDEX].orientation.z, rx_msg->pose[INDEX].orientation.w);
+	ROS_INFO("Linear      ( %f, %f, %f )", rx_msg->twist[INDEX].linear.x, rx_msg->twist[INDEX].linear.y, rx_msg->twist[INDEX].linear.z);
+	ROS_INFO("Angular     ( %f, %f, %f )", rx_msg->twist[INDEX].angular.x, rx_msg->twist[INDEX].angular.y, rx_msg->twist[INDEX].angular.z);
+
+	float x_vel = rx_msg->twist[INDEX].linear.x;
+	float y_vel = rx_msg->twist[INDEX].linear.y;
+	float z_vel = rx_msg->twist[INDEX].linear.z;
+	float x_pos = rx_msg->pose[INDEX].position.x;
+	float y_pos = rx_msg->pose[INDEX].position.y;
+	float vel_2 = x_vel*x_vel + y_vel*y_vel + z_vel*z_vel;
+	
+	// Set rover forces here and publish them to rover_forces
+	tx_msg->fl_wheel_wrench = 0;
+	tx_msg->ml_wheel_wrench = 0;
+	tx_msg->rl_wheel_wrench = 0;
+	tx_msg->rr_wheel_wrench = 0;
+	tx_msg->mr_wheel_wrench = 0;
+	tx_msg->fr_wheel_wrench = 0;
+	tx_msg->fl_steer_wrench = 0;
+	tx_msg->rl_steer_wrench = 0;
+	tx_msg->rr_steer_wrench = 0;
+	tx_msg->fr_steer_wrench = 0;
+
+	ROS_INFO("Velocity^2 = %f \t Scale = %f \t X Vel %f", vel_2, scale, x_vel);
+	publisher.publish(tx_msg);
+}
+
+int main(int argc, char **argv) {
+	ros::init(argc, argv, "rover_ros_node");
+	ros::NodeHandle _nh("rover");
+
+	publisher = _nh.advertise<rover_test::RoverCommands>("rover_forces", 1000);
+	ros::Subscriber subscriber = _nh.subscribe("/gazebo/model_states", 1000, RoverStateCallback);
+
+	ros::spin();
+	return 0;
+}
---
+++ catkin_ws/src/rover_test/src/rover_ros_plugin.cpp
@@ -0,0 +1,138 @@
+#include <boost/bind.hpp>
+#include <gazebo/gazebo.hh>
+#include <gazebo/physics/physics.hh>
+#include <gazebo/common/common.hh>
+#include <gazebo/common/Plugin.hh>
+#include <ros/ros.h>
+
+namespace gazebo {
+
+	class RoverSimulator : public RoverPlugin {
+	
+		void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf) {
+		
+			// Make sure the ROS node for Gazebo has already been initialized
+			if (!ros::isInitialized()) {
+				ROS_FATAL_STREAM("A ROS node for Gazebo has not been initialized, unable to load plugin. "
+				<< "Load the Gazebo system plugin 'libgazebo_ros_api_plugin.so' in the gazebo_ros package)");
+				return;
+			}
+			
+			// Save Global Pointers
+			this->rover = _model;
+			this->sdf = _sdf;
+			this->world = rover->GetWorld();
+
+			this->fl_wheel_joint = rover->GetJoint("fl_wheel_joint");
+			this->ml_wheel_joint = rover->GetJoint("ml_wheel_joint");
+			this->rl_wheel_joint = rover->GetJoint("rl_wheel_joint");
+			this->fr_wheel_joint = rover->GetJoint("fr_wheel_joint");
+			this->mr_wheel_joint = rover->GetJoint("mr_wheel_joint");
+			this->rr_wheel_joint = rover->GetJoint("rr_wheel_joint");
+
+			this->fl_steer_joint  = rover->GetJoint("fl_bogie_wheel_joint");
+			this->rl_steer_joint  = rover->GetJoint("left_rocker_wheel_joint");
+			this->fr_steer_joint  = rover->GetJoint("fr_bogie_wheel_joint");
+			this->rr_steer_joint  = rover->GetJoint("right_rocker_wheel_joint");
+
+			this->fl_wheel_wrench  = 0;
+			this->ml_wheel_wrench  = 0;
+			this->rl_wheel_wrench  = 0;
+			this->fr_wheel_wrench  = 0;
+			this->mr_wheel_wrench  = 0;
+			this->rr_wheel_wrench  = 0;
+			
+			this->fl_steer_wrench  = 0;
+			this->rl_steer_wrench  = 0;
+			this->fr_steer_wrench  = 0;
+			this->rr_steer_wrench  = 0;
+
+			this->_spinnerThread = boost::thread(boost::bind(&RoverSimulator::LoadThread, this));
+		}
+
+		void LoadThread() {
+		
+			if(!this->rover) {
+				ROS_FATAL_STREAM("Rover plugin requires a rover as its parent.");
+				return;
+			}
+			
+			ros::NodeHandle _nh("rover");
+			subscriber = _nh.subscribe("rover_forces", 1000, &RoverSimulator::RoverStateCallback, this);
+			this->_updateConnection = event::Events::ConnectWorldUpdateBegin(boost::bind(&RoverSimulator::OnUpdate, this, _1));
+			ROS_INFO("Rover plugin for ROS successfully loaded :)");
+
+			// Check for new messages
+			//      ros::spin();
+			while(ros::ok()) {
+				ros::spinOnce();
+			}
+		}
+
+		// Called by the world update start event
+		void OnUpdate(const common::UpdateInfo & /*_info*/) {
+			if ( subscriber.getNumPublishers() != 0 ) {
+				this->fl_wheel_joint->SetForce(0,fl_wheel_wrench);
+				this->ml_wheel_joint->SetForce(0,ml_wheel_wrench);
+				this->rl_wheel_joint->SetForce(0,rl_wheel_wrench);
+				this->rr_wheel_joint->SetForce(0,rr_wheel_wrench);
+				this->mr_wheel_joint->SetForce(0,mr_wheel_wrench);
+				this->fr_wheel_joint->SetForce(0,fr_wheel_wrench);
+				
+				this->fl_steer_joint->SetForce(0,fl_steer_wrench);
+				this->rl_steer_joint->SetForce(0,rl_steer_wrench);
+				this->rr_steer_joint->SetForce(0,rr_steer_wrench);
+				this->fr_steer_joint->SetForce(0,fr_steer_wrench);
+			}
+			else {
+				this->fl_wheel_joint->SetForce(0,0);
+				this->ml_wheel_joint->SetForce(0,0);
+				this->rl_wheel_joint->SetForce(0,0);
+				this->rr_wheel_joint->SetForce(0,0);
+				this->mr_wheel_joint->SetForce(0,0);
+				this->fr_wheel_joint->SetForce(0,0);
+				
+				this->fl_steer_joint->SetForce(0,0);
+				this->rl_steer_joint->SetForce(0,0);
+				this->rr_steer_joint->SetForce(0,0);
+				this->fr_steer_joint->SetForce(0,0);
+			}
+		}
+		
+		void RoverStateCallback(const geometry_msgs::Quaternion::ConstPtr& rx_msg) {
+			front_left_wrench  = rx_msg->x;
+			front_right_wrench = rx_msg->y;
+			back_left_wrench = rx_msg->z;
+			back_right_wrench  = rx_msg->w;
+		}
+
+		// Global pointer declarations
+		private: physics::ModelPtr rover;
+		private: physics::WorldPtr world;
+		private: sdf::ElementPtr sdf;
+		private: event::ConnectionPtr _updateConnection;
+		private: boost::thread _spinnerThread;
+
+		private: physics::JointPtr fl_wheel_joint;
+		private: physics::JointPtr ml_wheel_joint;
+		private: physics::JointPtr rl_wheel_joint;
+		private: physics::JointPtr fr_wheel_joint;
+		private: physics::JointPtr mr_wheel_joint;
+		private: physics::JointPtr rr_wheel_joint;
+
+		private: physics::JointPtr fl_steer_joint;
+		private: physics::JointPtr rl_steer_joint;
+		private: physics::JointPtr fr_steer_joint;
+		private: physics::JointPtr rr_steer_joint;
+
+		private: float front_left_wrench;
+		private: float front_right_wrench;
+		private: float back_left_wrench;
+		private: float back_right_wrench;
+
+		private: ros::Subscriber subscriber;
+	};
+
+	GZ_REGISTER_MODEL_PLUGIN(RoverSimulator);
+}
+
