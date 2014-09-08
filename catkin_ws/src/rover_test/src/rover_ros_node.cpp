875d06b4f59e7e90b0df318725b6b892faf33f84
Alankar Kotwal <alankarkotwal13@gmail.com>
2014-09-08 16:47:56 +0530

    Rover model test tested for static case

--- catkin_ws/src/rover_test/src/rover_ros_node.cpp
+++ catkin_ws/src/rover_test/src/rover_ros_node.cpp
@@ -2,7 +2,7 @@
 #include <gazebo_msgs/ModelStates.h>
 #include <stdlib.h>
 #include <rover_test/RoverCommands.h>
-#define INDEX 0
+#define INDEX 1
 
 float t_vel_2 = 4;
 
@@ -10,14 +10,16 @@ ros::Publisher publisher;
 rover_test::RoverCommands tx_msg;
 
 void RoverStateCallback(const gazebo_msgs::ModelStates::ConstPtr& rx_msg) {
-	if (rx_msg->name[INDEX] != "rover") { // TODO: Search for rover in general
+	if (rx_msg->name[INDEX] != "Rover") { // TODO: Search for rover in general
 		ROS_ERROR("Rover not found at specified index in '/gazebo/model_states'");
 		exit (EXIT_FAILURE);
 	}
-	ROS_INFO("Position    ( %f, %f, %f )", rx_msg->pose[INDEX].position.x, rx_msg->pose[INDEX].position.y, rx_msg->pose[INDEX].position.z);
-	ROS_INFO("Orientation ( %f, %f, %f, %f )", rx_msg->pose[INDEX].orientation.x, rx_msg->pose[INDEX].orientation.y, rx_msg->pose[INDEX].orientation.z, rx_msg->pose[INDEX].orientation.w);
-	ROS_INFO("Linear      ( %f, %f, %f )", rx_msg->twist[INDEX].linear.x, rx_msg->twist[INDEX].linear.y, rx_msg->twist[INDEX].linear.z);
-	ROS_INFO("Angular     ( %f, %f, %f )", rx_msg->twist[INDEX].angular.x, rx_msg->twist[INDEX].angular.y, rx_msg->twist[INDEX].angular.z);
+	//ROS_INFO("Position    ( %f, %f, %f )", rx_msg->pose[INDEX].position.x, rx_msg->pose[INDEX].position.y, rx_msg->pose[INDEX].position.z);
+	//ROS_INFO("Orientation ( %f, %f, %f, %f )", rx_msg->pose[INDEX].orientation.x, rx_msg->pose[INDEX].orientation.y, rx_msg->pose[INDEX].orientation.z, rx_msg->pose[INDEX].orientation.w);
+	//ROS_INFO("Linear      ( %f, %f, %f )", rx_msg->twist[INDEX].linear.x, rx_msg->twist[INDEX].linear.y, rx_msg->twist[INDEX].linear.z);
+	//ROS_INFO("Angular     ( %f, %f, %f )", rx_msg->twist[INDEX].angular.x, rx_msg->twist[INDEX].angular.y, rx_msg->twist[INDEX].angular.z);
+
+	ROS_INFO("Model running.");
 
 	float x_vel = rx_msg->twist[INDEX].linear.x;
 	float y_vel = rx_msg->twist[INDEX].linear.y;
@@ -33,10 +35,11 @@ void RoverStateCallback(const gazebo_msgs::ModelStates::ConstPtr& rx_msg) {
 	tx_msg.rr_wheel_wrench = 0;
 	tx_msg.mr_wheel_wrench = 0;
 	tx_msg.fr_wheel_wrench = 0;
-	tx_msg.fl_steer_wrench = 0;
-	tx_msg.rl_steer_wrench = 0;
-	tx_msg.rr_steer_wrench = 0;
-	tx_msg.fr_steer_wrench = 0;
+	
+	tx_msg.fl_steer_wrench = 100;
+	tx_msg.rl_steer_wrench = 100;
+	tx_msg.rr_steer_wrench = 100;
+	tx_msg.fr_steer_wrench = 100;
 
 	ROS_INFO("Velocity^2 = %f \t X Vel %f", vel_2, x_vel);
 	publisher.publish(tx_msg);
